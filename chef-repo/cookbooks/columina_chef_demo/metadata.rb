1f59f5c33912924e23d3529eec5610f1d5160a28
Ben Sullivan <office@bensullivan.net>
2013-02-05 08:23:49 +1000

    Added powershell cookbook dependency

--- chef-repo/cookbooks/columina_chef_demo/metadata.rb
+++ chef-repo/cookbooks/columina_chef_demo/metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures columina_chef_demo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+depends          "powershell"
 version          "0.0.1"
